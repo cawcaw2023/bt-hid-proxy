1ac8249d9af4b9a39a4ee0efc9d067d4f7910465
cawcaw2023 <131176392+cawcaw2023@users.noreply.github.com>
2024-03-19 20:16:23 +0900

    Support non-US Keybord layout.

--- setup.sh
+++ setup.sh
@@ -25,6 +25,11 @@ wget https://raw.githubusercontent.com/mtlynch/ansible-role-key-mime-pi/master/f
   -O "$ENABLE_RPI_HID_PATH"
 chmod +x "$ENABLE_RPI_HID_PATH"
 
+# For NON-US Keyboard support. Modify in the 53rd/59th byte from "x65" to "xff".
+# This means that the maximum acceptable keycode has been extended from 101 to 255.
+sed -i -e 's@x65\\\\x05\\\\x07\\\\x19\\\\x00\\\\x29\\\\x65@xff\\\\x05\\\\x07\\\\x19\\\\x00\\\\x29\\\\xff@g' \
+  $ENABLE_RPI_HID_PATH
+
 pushd $(mktemp -d)
 wget https://raw.githubusercontent.com/mtlynch/ansible-role-key-mime-pi/master/templates/usb-gadget.systemd.j2
 sed -e "s@{{ key_mime_pi_initialize_hid_script_path }}@${ENABLE_RPI_HID_PATH}@g" \
